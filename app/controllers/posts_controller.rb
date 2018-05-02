4537daf74cd7faae497d2212030058ebe3e2389c
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-05-02 22:22:37 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,14 +9,13 @@ class PostsController < ApplicationController
 
   def update
     @post = Post.find(params[:id])
-    if @post.update(post_params).valid?
-      @post.update(post_params)
-      redirect_to post_path(@post)
-    else 
-      render :edit
-    end
-  end
 
+   if @post.update(post_params)
+     redirect_to post_path(@post)
+   else
+     render :edit
+   end
+ end
   private
 
   def post_params
